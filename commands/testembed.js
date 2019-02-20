6affcac029b9b0b4b52d89123962c6aab5949fe2
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-02-21 01:14:05 +0800

    Create testembed.js

---
+++ commands/testembed.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  
+}
+
+
+module.exports.config = {
+
+  name: "test2",
+  aliases: ["test"],
+  description: "Testing",
+  usage: "derp>test",
+  noalias: "No Aliases",
+  accessableby: "Members"
+}
