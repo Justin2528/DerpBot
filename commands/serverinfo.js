306dd1032360f435e5af30a007dbf0ae995ba25c
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-02-20 19:18:45 +0800

    Create serverinfo.js

---
+++ commands/serverinfo.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  let sEmbed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .setTitle("server Info")
+  .setThumbnail(message.guild.iconURL)
+  .setAuthor(`${message.guild.name} Info`, message.guild.IconURL)
+  .addField("**Guild Name:**", `${message.guild.name}`, true)
+  .addField("**Guild Owner:**", `${message.guild.owner}`, true)
+  .addField("**Member Count:**", `${message.guild.memberCount}`, true)
+  .AddField("**Role Count**", `${message.guild.roles.size}`, true)
+  .setFooter(`DerpBot | Footer`, bot.user.displayAvatarURL)
+  message.channel.send(sEmbed)
+  
+}
+
+
+module.exports.config = {
+  name: "serverinfo",
+  aliases: ["si", "serverdesc"]
+
+}
