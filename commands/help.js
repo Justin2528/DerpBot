fd9b4b539542fa6826ac1099164cbe54e065a15f
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-02-21 00:54:59 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -30,7 +30,7 @@ module.exports.run = async (bot, message, args) => {
     .setThumbnail(bot.user.displayAvatarURL)
     .setTimestamp()
     .setDescription(`These are the avaliable commands for the DerpBot\n The bot prefix is: ${prefix}`)
-    .addField(`Commands:`, "``serverinfo`` ``help`")
+    .addField(`Commands:`, "``serverinfo`` `help`")
     .setFooter("DerpBot v0.3.9", bot.user.displayAvatarURL)
     message.channel.send(embed).then(m => m.delete(10000));
     message.author.send(Sembed);
