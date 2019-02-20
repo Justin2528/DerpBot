7b61105cb630903913aa75add9e4e345daa25272
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-02-20 19:12:02 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,31 @@ bot.on("ready", async () => {
     bot.user.setActivity("derp>help - DerpBot");
 })
 
+const fs = require("fs");
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection()
+
+fs.readdir("./commands/", (err, files) => {
+
+    if(err) console.log(err)
+    
+    let jsfile = files.filter(f => f.split(".").pop() === "js")
+    if(jsfile.length <= 0) {
+        console.log("[LOGS] No Javascript Files. Nothing Loaded.")
+    }
+    
+    jsfile.forEach((f, i) => {
+        let pull = require(`./commands/${f}`);
+        bot.commands.set(pull.config.name, pull);
+        pull.config.aliases.forEach(alias => {
+            bot.aliases.set(alias, pull.config.name)
+        })
+    })
+
+});
+
+
+
 bot.on("message", async message => {
   if(message.author.bot || message.channel.type === "dm") return;
   
