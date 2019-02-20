d5b161229444390a231701df531ef87b9831c5de
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-02-21 01:19:00 +0800

    Update testembed.js

--- commands/testembed.js
+++ commands/testembed.js
@@ -1,7 +1,19 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+  message.channel.send("DerpBot Test Done")
   
+  message.reply("Reply Test Done")
+  
+  let embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .setTitle("Test")
+  .setAuthor("Test")
+  .setDescription("Test")
+  .setFooter("Test")
+  .addField("Te","st", true)
+  
+  message.channel.send(embed)
 }
 
 
