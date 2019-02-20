4a85bf9075e4f47f093dcb0f2060272de894289d
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-02-20 18:46:08 +0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,24 @@
+const botconfig = require("botconfig.json");
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+    bot.user.setActivity("derp>help - DerpBot");
+})
+
+bot.on("message", async message => {
+  if(message.author.bot || message.channel.type === "dm") return;
+  
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ")
+  let cmd  = messageArray[0];
+  let args = messageArray.slice(1);
+  
+  if(cmd ===`${prefix}test`){
+    message.channel.send("Omg it work i cri omg ded not big surprise");
+  }
+  
+})
+
+
+bot.login(process.env.TOKEN)
