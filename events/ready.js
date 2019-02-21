0cb7f48d8879ee331e0914d77392c58a73bb493f
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-02-21 16:36:58 +0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,17 @@
 const Discord = require("discord.js");
 
 module.exports = bot => {
-   bot.user.setActivity("derp>help - DerpBot");
+//    bot.user.setActivity("derp>help - DerpBot");
+   
+   
+      let statuses = [
+         `derp>help - DerpBot`,
+         `Partner of EDGE! - DerpBot`,
+         `DerpBot version: v0.3.9 - DerpBot`
+      ]
+      setInterval(function() {
+      let status = statuses[Math.floor(Math.random() * statuses.length)]
+       bot.user.setActivity(status, {type: "WATCHING"});
+   
+   }, 3500)
 }
