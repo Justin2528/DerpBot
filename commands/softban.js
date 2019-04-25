084d06a6727af52bca154935d886a737492faf07
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-04-26 00:48:38 +0800

    Create softban.js

---
+++ commands/softban.js
@@ -0,0 +1,47 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+message.delete()
+  if(!message.member.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.reply(`||ERROR REPORT: ${message.author.username} HAVE NO BAN_MEMBERS PERMS.||`);
+     
+
+
+  let bane = message.mentions.members.first() || message.guild.members.get(args[0]);
+  
+  if(!bane) return message.reply(`Oh no! where is da bad boi? (if you don't understand, do d>help softban...)`);
+
+ if(bane.hasPermission("BAN_MEMBERS")) return message.channel.send("That boi got perms... ");
+  
+  let reason = args.slice(1).join(" ");
+  if(!reason) reason = "N̝̜̦̳̽͂͑̑o͇̻͈̓̋̀̓̐͟ͅ ͈̐ȑ͜e͗̀͟͢a̢̰̣̱̞͌̿̓̓͞s͙͚͔̓͌̓ò̲̳̏n̪͠ ̰̝͇̦͔̀͐̇͒͛g̞͈̭̙̋͌͗̅͟͞i̖̗͆̅v̹̐en!̮̦͉̈́̇̊"
+
+let time = args.slice(2).join(" ");
+
+if(!time) time = "1"
+
+
+if(!message.guild.me.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.reply("Oh, I have no perms to ban people.... (Insert sad face here)");
+
+bane.send(`Hello there, I'm sorry to tell you but you have been banned in ${message.guild.name} by ${message.author.username}. reason: ${reason}`).then(() =>
+  message.guild.ban(bane, {days: time, reason: reason})).then(() => message.guild.unban(bane.id)).catch(err => message.channel.send(err))
+
+message.channel.send(`**${bane.user.username}** has been banned by ${message.author.username}. reason: ${reason}`);
+
+
+
+  
+  
+  
+  }
+// end of module
+
+
+module.exports.config = {
+  name: "softban",
+  aliases: ["sb", "sbanish", "sremove"],
+  description: "Ban some bad boi with time",
+  usage: "d>softban <@user> <reason> <days>",
+  noalias: "No Aliases",
+  accessableby: "Staff(Need BAN_MEMBERS perms)/Admin"
+}
+
