dc41b4011768b1c5a05ff198689aff78014591da
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-04-26 18:53:33 +0800

    Create unban.js

---
+++ commands/unban.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  
+    
+  
+}
+
+
+module.exports.config = {
+  name: "ban",
+  aliases: ["ub", "unbanish"],
+  description: "Unban da bad boi",
+  usage: "d>unban <userID> <reason (optional)> || Why userID? Because we unban the guy from OUTSIDE the guild/server. The bot don't know who the guy is because it's outside the guild/server. ",
+  noalias: "No Aliases",
+  accessableby: "Staff (BAN_MEMBERS) || Admin"
+}
