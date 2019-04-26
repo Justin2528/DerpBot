2af0bf7718059b56e0d87031f21c7eabb77fe3e3
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-04-27 00:31:42 +0800

    Create cool.js

---
+++ commands/cool.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  
+}
+
+
+module.exports.config = {
+  name: "cool",
+  aliases: ["c", "col"],
+  description: "Uh cool?",
+  usage: "d>cool",
+  noalias: "No Aliases",
+  accessableby: "Members"
+}
