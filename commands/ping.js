d211035039ec8291f3654c6870e03f7d3ce00ff2
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-04-27 00:26:31 +0800

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
 
     message.channel.send("Pinging...").then(m => {
         let ping = m.createdTimestamp - message.createdTimestamp
-        let choices = ["Is this really my ping", "Is it okay? I cant look", "I hope it isnt bad", ""]
+        let choices = ["Is this really my ping", "Is it okay? I cant look", "I hope it isnt bad", "I think it's ok?", "Roses are red, violets are blue, pings are bad"]
         let response = choices[Math.floor(Math.random() * choices.length)]
 
         m.edit(`${response}: Bot Latency: \`${ping}\`, API Latency: \`${Math.round(bot.ping)}\``)
