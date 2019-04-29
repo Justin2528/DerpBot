7100fa46c3ce0a138ff34c514901d2a55b6c9da6
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-04-30 01:31:39 +0800

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -1,17 +1,35 @@
-const Discord = require("discord.js");
+const Discord = require("discord.js")
+const botconfig = require("../botconfig.json");
+
+
 
 module.exports.run = async (bot, message, args) => {
-  
-    
-  
-}
 
+    if(!message.member.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("You dont have permission to perform this command!")
+
+    let bannedMember = await bot.fetchUser(args[0])
+        if(!bannedMember) return message.channel.send("Please provide a user id to unban someone!")
+
+    let reason = args.slice(1).join(" ")
+        if(!reason) reason = "No reason given!"
+
+    if(!message.guild.me.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("I dont have permission to perform this command!")|
+    message.delete()
+    try {
+        message.guild.unban(bannedMember, {reason: reason})
+        message.channel.send(`${bannedMember.tag} has been unbanned from the guild!`)
+    } catch(e) {
+        message.channel.send(e.message)
+    }
+bannedMember.send(`You have been unbanned by ${message.author.username} in ${message.guild.name}. reason: ||${reason}||`)
+
+}
 
 module.exports.config = {
-  name: "unban",
-  aliases: ["ub", "unbanish"],
-  description: "Unban da bad boi",
-  usage: "d>unban <userID> <reason (optional)> || Why userID? Because we unban the guy from OUTSIDE the guild/server. The bot don't know who the guy is because it's outside the guild/server. ",
-  noalias: "No Aliases",
-  accessableby: "Staff (BAN_MEMBERS) || Admin"
+    name: "unban",
+    description: "Unban a user from the guild!",
+    usage: "d>unban <USER ID <--- ID not MENTION> <reason>",
+    accessableby: "Administrators",
+  aliases: ["ub", "unbanish"]
 }
+   
