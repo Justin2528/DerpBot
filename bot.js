52bdf49eaf764e56abb1084bd8965c3b50385122
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-05-01 17:23:57 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const botconfig = require("./botconfig.json");
+const YTDL = require("ytdl-core")
 const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 let prefix = botconfig.prefix
@@ -12,6 +13,8 @@ bot.aliases = new Discord.Collection()
 fs.readdir("./commands/", (err, files) => {
 
     if(err) console.log(err)
+    
+    var servers = {}
     
     let jsfile = files.filter(f => f.split(".").pop() === "js")
     if(jsfile.length <= 0) {
@@ -50,6 +53,37 @@ if(!message.content.startsWith(prefix)) return;
     message.channel.send("Omg it work i cri omg ded not big surprise");
   }
   
+    if(cmd ===`${prefix}play`) {
+    
+
+
+function play(connection, message) {
+  var server = servers[message.guild.id];
+
+  server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+  server.queue.shift();
+
+  server.dispatcher.on("end", function () {
+   if (server.queue[0]) play(connection, message);
+   else connection.disconnect();
+});
+
+}
+  if(!args[1]) {
+    message.channel.send("Gimme da music link.");
+  }
+  if(!message.member.voiceChannel) {
+      message.channel.send("You are not in a voice channel...");
+   }
+  if(!servers[message.guild.id]) servers[message.guild.id] = {
+    queue: []
+   };
+  var server = servers[message.guild.id];
+
+  if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
+      play(connection, message);
+  });
+    }
 })
 
 
