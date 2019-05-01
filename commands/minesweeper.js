1fb34a29c81b1d92708398f173e2b6d56f398d10
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-05-01 13:03:34 +0800

    Update minesweeper.js

--- commands/minesweeper.js
+++ commands/minesweeper.js
@@ -5,10 +5,29 @@ module.exports.run = async (bot, message, args) => {
 
 // Returns a Discord-interpretable string with a 9x9 matrix of spoilers and emojis, 10 of which are mines.
  
-const minesweeper = new Minesweeper()
-minesweeper.start();
-}
-
+ const rows = parseInt(args[0]);
+    const columns = parseInt(args[1]);
+    const mines = parseInt(args[2]);
+ 
+    if (!rows) {
+      return message.channel.send(':warning: Please provide the number of rows.');
+    }
+ 
+    if (!columns) {
+      return message.channel.send(':warning: Please provide the number of columns.');
+    }
+ 
+    if (!mines) {
+      return message.channel.send(':warning: Please provide the number of mines.');
+    }
+ 
+    const minesweeper = new Minesweeper({ rows, columns, mines });
+    const matrix = minesweeper.start();
+ 
+    return matrix
+      ? message.channel.send(matrix)
+      : message.channel.send(':warning: You have provided invalid data.');
+  }
 
 module.exports.config = {
   name: "minesweeper",
