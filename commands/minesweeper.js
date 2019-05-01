1fa449b270215b36c7cc4545e81370665528647b
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-05-01 12:53:49 +0800

    Update minesweeper.js

--- commands/minesweeper.js
+++ commands/minesweeper.js
@@ -2,17 +2,10 @@ const Discord = require("discord.js");
 const Minesweeper = require('discord.js-minesweeper');
 
 module.exports.run = async (bot, message, args) => {
-  const minesweeper = new Minesweeper();
-minesweeper.start();
+
 // Returns a Discord-interpretable string with a 9x9 matrix of spoilers and emojis, 10 of which are mines.
  
-const minesweeper = new Minesweeper({
-  rows: 12,
-  columns: 16,
-  mines: 20,
-  emote: 'boom',
-  returnType: 'code',
-});
+const minesweeper = new Minesweeper()
 minesweeper.start();
 }
 
