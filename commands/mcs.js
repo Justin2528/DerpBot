2aa3cc881dcbfe8ecc5bd79c921c76128c0b9849
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-05-02 17:12:38 +0800

    Update mcs.js

--- commands/mcs.js
+++ commands/mcs.js
@@ -2,12 +2,14 @@ const Discord = require('discord.js');
 
 
 var request = require('request');
-var mcCommand = '/mcs'; // Command for triggering
-var mcIP = args[0]; // Your MC server IP
-var mcPort = 25565; // Your MC server port (25565 is the default)
+ // Your MC server port (25565 is the default)
 
 
 module.exports.run = async (bot, message, args) => {
+var mcCommand = '/mcs'; // Command for triggering
+var mcIP = args[0]; // Your MC server IP
+var mcPort = 25565;
+
    if (!message.content === mcCommand) return message.channel.send("Not /mcs");
     if (message.content === mcCommand) {
         var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort;
