9fdd1d67f45145b85a9cd2d235a0c89d0514ff28
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-05-02 17:05:00 +0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -7,7 +7,7 @@ module.exports = bot => {
       let statuses = [
          `d>help - DerpBot`,
          `Partner of EDGE! - DerpBot`,
-         `DerpBot version: v1.0 - DerpBot`
+         `DerpBot version: v1.0.2 - DerpBot`
       ]
       setInterval(function() {
       let status = statuses[Math.floor(Math.random() * statuses.length)]
