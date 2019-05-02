a451374a6c3939afddb4d49ee58e6e92cfdb10f3
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-05-02 17:07:14 +0800

    Create mcs.js

---
+++ commands/mcs.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+
+
+var request = require('request');
+var mcCommand = '/mcs'; // Command for triggering
+var mcIP = args[0]; // Your MC server IP
+var mcPort = 25565; // Your MC server port (25565 is the default)
+
