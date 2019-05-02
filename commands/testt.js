9a1fb229d1745baa091ddec2afeddef16d5019d7
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-05-02 17:53:36 +0800

    Update testt.js

--- commands/testt.js
+++ commands/testt.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
    message.channel.send("Hello there!")
      const filter = m => m.author.id === message.author.id;
-    message.reply(" Tell us ur name! Will expire in 10 seconds... ("Or type cancel")").then(r => r.delete(10000));
+    message.reply(" Tell us ur name! Will expire in 10 seconds... (Or type cancel)").then(r => r.delete(10000));
     message.channel.awaitMessages(filter, {max: 1, time: 10000}).then(collected => {
 
         if(collected.first().content === cancel){
