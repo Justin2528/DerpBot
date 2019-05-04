5349e3bd08eb1d7a6eab8fac49706574e2781177
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-05-04 23:17:52 +0800

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  message.channel.send(`[Ok here]()https://discordapp.com/api/oauth2/authorize?client_id=547771680798539776&permissions=8&scope=bot`);
+  message.channel.send(`[Ok here](https://discordapp.com/api/oauth2/authorize?client_id=547771680798539776&permissions=8&scope=bot)`);
 }
 
 
