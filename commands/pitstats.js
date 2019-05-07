c712a18a3a99ca868076d20d3c642cd2ef63e181
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-05-07 19:03:43 +0800

    Update pitstats.js

--- commands/pitstats.js
+++ commands/pitstats.js
@@ -5,12 +5,30 @@ module.exports.run = async (bot, message, args) => {
 
  var IGN = args[0];
  
+if(!IGN) return message.channel.send("Ok, what IS UR IGN????")
+
  var url = "https://api.hypixel.net/player?key=12986a35-ab5a-49f4-b23c-e6695ae3cfd1&name=" + IGN;
 
 snekfetch.get(url).then(r => {
 let body = r.body
-});
-  
+
+let ok = new Discord.RichEmbed()
+.setTitle("Pit stats (Hypixel)")
+.setColor("9b7653")
+.setDescription(`${IGN}'s Pit stats`)
+.addField("Play time (mins)", body.player.pit_stats_ptl.playtime_minutes)
+.addField("Enderchest open times", body.player.pit_stats_ptl.enderchest_opened)
+.addField("Joins", body.player.pit_stats_ptl.joins)
+.addField("Diamond Items purchased", body.player.pit_stats_ptl.diamond_items.purchased)
+.addField("Chat Messages", body.player.pit_stats_ptl.chat_message)
+.addField("Gold earned", body.player.pit_stats_ptl.cash_earned)
+.addField("Fishing Rod Launched", body.player.pit_stats_ptl.fishing_rod_launched)
+.setFooter("We're adding more!")
+
+message
+})else{
+message.channel.send("Nope... no stats for u cus u don't play pit...");
+}
 }
 
 
