47992a09ad055abd410e854a27fd2bf6d2fc85de
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-05-11 21:00:44 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -21,7 +21,7 @@ if(args[0] == "cool") return message.channel.send("No such command call `cool`")
     .setTimestamp()
     .setDescription(`Games || DerpBot\n The bot prefix is: ${prefix}`)
     .addField(`Games`, "minesweeper")
-        .setFooter("DerpBot v1.0.2", bot.user.displayAvatarURL)
+        .setFooter("DerpBot v1.1", bot.user.displayAvatarURL)
      message.channel.send(gembed).then(m => m.delete(10000));
     return message.author.send(Gembed);
     }
@@ -39,7 +39,7 @@ if(args[0] == "cool") return message.channel.send("No such command call `cool`")
     .setDescription(`Beta Commands || DerpBot\n The bot prefix is: ${prefix}`)
     .addField(`Commands`,' `welp no commands are here...`')
     .addField(`Note:` , `Tell JuStIn2528#9111 if there was something wrong with the command!`)
-        .setFooter("DerpBot v1.0.2", bot.user.displayAvatarURL)
+        .setFooter("DerpBot v1.1", bot.user.displayAvatarURL)
      message.channel.send(eembed).then(m => m.delete(10000));
     return message.author.send(Bembed);
     }
@@ -53,7 +53,7 @@ if(args[0] == "cool") return message.channel.send("No such command call `cool`")
       .setColor("RANDOM")
       .setAuthor(`DerpBot Help`, message.guild.iconURL)
       .setDescription(`The bot prefix is: ${prefix}\n\n**Command:** ${command.config.name}\n**Description:** ${command.config.description || "No Description"}\n**Usage:** ${command.config.usage || "No Usage"}\n **Accessable by:** ${command.config.accessableby || "Members"}\n**Aliases:** ${command.config.noalias || command.config.aliases}`)
-     .setFooter("DerpBot v1.0.2", bot.user.displayAvatarURL)
+     .setFooter("DerpBot v1.1", bot.user.displayAvatarURL)
     
       return message.channel.send(SHembed)
       }}
@@ -75,7 +75,7 @@ if(args[0] == "cool") return message.channel.send("No such command call `cool`")
     .addField(`Commands:`, "``serverinfo`` `help` `test2` `rps` `mute` `ban` `softban` `unban` `kick` `ping` `uptime` `???` `yesorno` `meme` `purge` `eval` `reload` `cat` `dog` `pitstats` `minesweeper` `invite` `npm` `1vs1`")
     .addField(`Psst!`, `Do d>help games for games!`)
     .addField(`Are you a tester?`, `Do d>help beta for some commands that are not release!`)
-        .setFooter("DerpBot v1.0.2", bot.user.displayAvatarURL)
+        .setFooter("DerpBot v1.1", bot.user.displayAvatarURL)
     message.channel.send(embed).then(m => m.delete(10000));
     message.author.send(Sembed);
   }
