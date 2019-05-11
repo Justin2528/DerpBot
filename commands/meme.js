e882f7189b283dde369f57efb04f7b40ab28b603
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-05-11 09:33:02 +0800

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -1,37 +1,19 @@
 const Discord = require("discord.js")
 const botconfig = require("../botconfig.json");
-const superagent = require("superagent")
+const superagent = require("superagent");
+const meme = require("memejs");
 
 
 module.exports.run = async (bot, message, args) => {
 
-if(!args[0]) return message.channel.send("img/text?");
-    let msg = await message.channel.send("Generating...")
+  meme(function(data) {
+  const embed = new Discord.RichEmbed()
+  .setTitle(data.title[0])
+  .setColor("RANDOM")
+  .setImage(data.url[0])
+  message.channel.send({embed});
+  })};
 
-    let {body} = await superagent
-    .get(`https://api-to.get-a.life/meme`)
-    //console.log(body.file)
-    if(!{body}) return message.channel.send("I broke! Try again.")
-
-        let mEmbed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setAuthor(`MEMES!`, message.guild.iconURL)
-        .setImage(body.url)
-        .setTimestamp()
-        .setFooter(`Meme 101`, bot.user.displayAvatarURL)
-
-       let tEmbed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setAuthor(`MEMES!`, message.guild.iconURL)
-       .setDescription(body.text)
-        .setTimestamp()
-        .setFooter(`Meme 101`, bot.user.displayAvatarURL)
-
-        msg.delete();
-
-
-   if(args[0] == "img") return msg.edit(mEmbed)
-   if(args[0] == "text") return msg.edit(tEmbed)
    
 
 }
