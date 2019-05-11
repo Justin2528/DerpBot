b20989c2767d64afc226d05214fa914a7adcb1c7
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-05-11 17:26:23 +0800

    Update pitstats.js

--- commands/pitstats.js
+++ commands/pitstats.js
@@ -18,7 +18,7 @@ const snekfetch = require("snekfetch");
 snekfetch.get(url).then(r => {
 let body = r.body
 
-if(!body.player.stats.Pit) return message.channel.send("ERROR 404: Uh, U never player pit? (hypixel pit)")
+if(!body.player.stats.Pit) return message.channel.send(`ERROR 404: Uh, ${IGN} never player pit? (hypixel pit)`)
 
 let image = "https://visage.surgeplay.com/full/" + body.player.uuid +".png"
 
