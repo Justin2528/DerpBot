69f9cf47468ee29de38b56a349c4e68e6e28f70e
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-05-16 20:28:30 +0800

    Update bedwarstats.js

--- commands/bedwarstats.js
+++ commands/bedwarstats.js
@@ -18,13 +18,14 @@ const snekfetch = require("snekfetch");
 snekfetch.get(url).then(r => {
 let body = r.body
 
-let offon = `${IGN} is offline!`
+let offon = `${body.player.displayName} is offline!`
 let photo = "https://images-ext-1.discordapp.net/external/UdWXoKUsaYCOyvH6nsDk03AdfJVWTK7cyQVvhqpoF3I/https/image.ibb.co/hwheRV/image.png"
 
 if(body.player.lastLogout < body.player.lastLogin){
-  offon = `${IGN} is online!`
+  offon = `${body.player.displayName} is online!`
   photo = "https://images-ext-1.discordapp.net/external/74xyeTgd4BElwkkoe92yB3zEwD1ln4N1rh2zeKxVwt8/https/image.ibb.co/h9VNfq/image.png"
 } 
+
 
 if(!body.player.stats.Bedwars) return message.channel.send(`ERROR 404: Uh, ${IGN} never player bedwars? (hypixel bewars)`)
 
@@ -38,7 +39,7 @@ console.log(body)
 let ok = new Discord.RichEmbed()
 .setTitle("Bedwars stats (Hypixel)")
 .setColor("9b7653")
-.setDescription(`${IGN}'s Bedwars stats`)
+.setDescription(`${body.displayName}'s Bedwars stats`)
 .setImage(image)
 .addField("EXP", body.player.stats.Bedwars.Experience || "0", true)
 .addField("Level", body.player.achievements.bedwars_level || "0", true)
