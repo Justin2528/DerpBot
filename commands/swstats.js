961b63f7ffad25cd79943b787e0b6879995157ad
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-05-18 21:43:12 +0800

    Update swstats.js

--- commands/swstats.js
+++ commands/swstats.js
@@ -45,6 +45,7 @@ let ok = new Discord.RichEmbed()
 .addField("Souls", body.player.stats.SkyWars.souls || "0", true)
 .addField("Tokens", body.player.stats.SkyWars.cosmetic_tokens || "0", true)
 .addField("Wins", body.player.stats.SkyWars.wins || "0", true)
+
 .setThumbnail("https://hypixel.net/styles/hypixel-uix/hypixel/game-icons/Skywars-64.png")
 .setFooter(offon, photo)
 
