1f029ec919ea758b9a0d77d54464a27d66c761bb
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-05-22 01:08:38 +0800

    Update watchdogstats.js

--- commands/watchdogstats.js
+++ commands/watchdogstats.js
@@ -12,14 +12,14 @@ snekfetch.get(url).then(r => {
     let body = r.body
 
     let oke = body.watchdog_total+body.staff_total
-
+    let photo = "https://images-ext-1.discordapp.net/external/KiCZjFl7Ph0aD0Sy3-1EAADDbRLE8k6NayWFH6olu5o/%3Fs%3D200%26v%3D4/https/avatars0.githubusercontent.com/u/3840546"
     let ok = new Discord.RichEmbed()
-   .setAuthor("Hypixel WatchDog Statistics", "https://images-ext-1.discordapp.net/external/KiCZjFl7Ph0aD0Sy3-1EAADDbRLE8k6NayWFH6olu5o/%3Fs%3D200%26v%3D4/https/avatars0.githubusercontent.com/u/3840546")
+   .setAuthor("Hypixel WatchDog Statistics")
     .setColor("RED")
    .addField("**Total Bans**", `Bans: \`${oke}\` \n WatchDog Bans: \`${body.watchdog_total}\` \n Staff Bans: \`${body.staff_total}\``, true)
   .addField("**Todays Bans**", `WatchDog Bans: \`${body.watchdog_rollingDaily}\` \n Staff Bans: \`${body.staff_rollingDaily}\` \n Last Minute: \`${body.watchdog_lastMinute}\``, true)
     .setThumbnail("https://vignette.wikia.nocookie.net/hypixelserver/images/9/94/Watchdog.png")
-    .setFooter("Don't think Watchdog is weak!")
+    .setFooter("Don't think Watchdog is weak!", photo)
 
 message.channel.send(ok)
     
