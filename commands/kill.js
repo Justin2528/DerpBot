bd839ece2d3dec72d21fb38ab2f3cacbd5836b47
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-05-21 21:36:37 +0800

    Update kill.js

--- commands/kill.js
+++ commands/kill.js
@@ -8,9 +8,9 @@ if(args[0] == "derpbot"){
    return message.channel.send("Am unkillable")
 }
 
-    let replies = ["was killed by the annoying dog.", "was killed by Justin2528.", "was killed by Shrey87.", "was killed by DerpBot", "has been banned from the server.", "was killed by a Polar Bear.", "committed die.", "got distracted by looking at a dandelion and ran into a pit of lava."];
+    let replies = ["tried to swim in lava.", "was killed by the annoying dog.", "was killed by Justin2528.", "was killed by Shrey87.", "was killed by DerpBot", "has been banned from the server.", "was killed by a Polar Bear.", "committed die.", "got distracted by looking at a dandelion and ran into a pit of lava."];
 let result = Math.floor((Math.random() * replies.length));
-    message.channel.send(`${args[0]} ${result}`)
+    message.channel.send(`${args[0]} ${replies[result])}`)
 }
 
 
