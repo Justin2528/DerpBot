af263456eff95f5d8002ba8e986511db1726ab36
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-05-21 18:45:00 +0800

    Create createinvite.js

---
+++ commands/createinvite.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    if (!message.member.hasPermission("CREATE_INSTANT_INVITE")) return message.channel.send("Nope");
+  message.channel.createInvite({maxAge: 0}).then(invite => {
+    let embed = new Discord.RichEmbed()
+    .setColor(color)
+    .setDescription(`**Permanent Invite Link**: ${invite}`);
+    message.channel.send(embed);
+  });
+}
+}
+
+
+module.exports.config = {
+  name: "createinvite",
+  aliases: ["cinvite"],
+  description: "Create da gud Invite link",
+  usage: "d>createinvite",
+  noalias: "No Aliases",
+  accessableby: "Members"
+}
