f8b3033ca5d3504232ed56f3b212cef239b337d0
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-05-21 21:32:52 +0800

    Create kill.js

---
+++ commands/kill.js
@@ -0,0 +1,24 @@
+
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+     if(!args[0]) return message.reply("Incorrect Usage! d>!kill <folks>")
+
+if(args[0] == "Derpbot" || "derpbot"){
+   return message.channel.send("Am unkillable")
+}
+
+    let replies = ["was killed by the annoying dog.", "was killed by Justin2528.", "was killed by Shrey87.", "was killed by DerpBot", "has been banned from the server.", "was killed by a Polar Bear.", "committed die.", "got distracted by looking at a dandelion and ran into a pit of lava."];
+let result = Math.floor((Math.random() * replies.length));
+    message.channel.send(`${args[0]} ${result}`)
+}
+
+
+module.exports.config = {
+  name: "kill",
+  aliases: ["murder"],
+  description: "You IDIOT, why you killed dat boi.",
+  usage: "d>kill <stuff or mention>",
+  noalias: "No Aliases",
+  accessableby: "Chara"
+}
