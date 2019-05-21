bba16b7e805f79f5a7d0810e1da9fed912a53de9
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-05-22 00:53:16 +0800

    Create watchdogstats.js

---
+++ commands/watchdogstats.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");
+
+
+module.exports.run = async (bot, message, args) => {
+
+var url = "https://api.hypixel.net/watchdogstats?key=" + process.env.APIKEY
+
+const snekfetch = require("snekfetch");    
+
+
+snekfetch.get(url).then(r => {
+    let body = r.body
+
+    let oke = body.watchdog_total+body.staff.total
+
+    let ok = new Discord.RichEmbed()
+   .setAuthor("Hypixel WatchDog Statistics")
+   .addField("**Total Bans**", `Bans: `${oke}` \n WatchDog Bans: `${body.watchdog_total}` \n Staff Bans: `${body.staff_total}``)|
+  .addField("**Todays Bans**", `WatchDog Bans: `${watchdog_rollingDaily}` \n Staff Bans: `${staff_rollingDaily}` \n Last Minute: `${watchdog_lastMinute}``)
+    .setThumbnail("https://vignette.wikia.nocookie.net/hypixelserver/images/9/94/Watchdog.png")
+    .setFooter("Wow...")
+
+message.channel.send(ok)
+    
+   });
+}
+
+
+module.exports.config = {
+  name: "watchdogstats",
+  aliases: [""],
+  description: "Uh get it?",
+  usage: "d>watchdogstats",
+  noalias: "No Aliases",
+  accessableby: "Members"
+}
