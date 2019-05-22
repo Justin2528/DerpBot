5d297f9def77052d8ff532e44e1de30c2f17f5c7
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-05-23 01:04:51 +0800

    Create rate.js

---
+++ commands/rate.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  if(!args[0]) return message.reply("Can't rate air...")
+      let result = Math.floor(Math.random() * 100);
+
+
+  let rateboi = args.slice().join(" ");
+
+   if(result == 21){
+  return message.channel.send(`Rating: ${rateboi} is ∞/∞ 🌟}`)
+} else {
+    return message.channel.send(`Rating: ${rateboi} is ${result}/100}`)
+}
+
+}
+
+
+module.exports.config = {
+  name: "rate",
+  aliases: ["r"],
+  description: "rate ur stuff like pizza",
+  usage: "d>rate <stuff>",
+  noalias: "No Aliases",
+  accessableby: "Members"
+}
