08989e5155b9626913048a3973e7826f258ab46d
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-05-28 11:30:47 +0800

    Ajm da winner
    Yay

--- commands/status.js
+++ commands/status.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   message.channel.send("Computer Exam :DDDDDDDDDD Ez");
+
+  message.author.send("Also am showing my teacher how to code Javascript cus my teacher don't know how to code Javascript LOL");
 }
 
 
