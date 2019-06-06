14b00ed0b0b1ffc4ad73c88cbd033f248720c184
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-06-06 23:11:36 +0800

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -3,11 +3,12 @@ const moment = require('moment');
 
 const cooldown = new Set();
 module.exports.run = async (bot, message, args) => {
+    let boi = message.content.split(' ').slice(1).join(' ');
  message.delete()
     if (cooldown.has(message.author.id && message.guild.id)) {
         return message.channel.send('**[COOLDOWN]** Sending tickets has **5 Minutes** Cooldown!');
     }
-    if (args.length < 1) {
+    if (boi.length < 1) {
         return message.channel.send(`You must give me something to report first ${message.author}`);
     }
     cooldown.add(message.author.id && message.guild.id);
@@ -19,14 +20,14 @@ module.exports.run = async (bot, message, args) => {
     message.channel.send(`Hey, ${message.author}, we got your report! We will reply soon as possible! Here is the full ticket:`);
     const embed2 = new Discord.RichEmbed()
         .setAuthor(`Ticket from ${message.author.tag}`, message.author.displayAvatarURL)
-        .addField('Ticket:', `**Tickets's Author:** ${message.author.tag}\n**Server:** ${guild.name}\n**Full ticket:** ${message.content}`)
+        .addField('Ticket:', `**Tickets's Author:** ${message.author.tag}\n**Server:** ${guild.name}\n**Full ticket:** ${boi}`)
         .setThumbnail(message.author.displayAvatarURL)
         .setFooter(`${moment().format('MMMM Do YYYY, h:mm:ss a')}`)
         .setColor(16711728);
     message.channel.send(embed2);
     const embed = new Discord.RichEmbed()
         .setAuthor(`Ticket from ${message.author.tag}`, message.author.displayAvatarURL)
-        .addField('Ticket:', `**Report's Author:** ${message.author.tag}\n**Server:** ${guild.name}\n**Full report:** ${message.content}`)
+        .addField('Ticket:', `**Report's Author:** ${message.author.tag}\n**Server:** ${guild.name}\n**Full report:** ${boi}`)
         .setThumbnail(message.author.displayAvatarURL)
         .setColor("#ffd700")
         .setFooter(`${moment().format('MMMM Do YYYY, h:mm:ss a')}`);
