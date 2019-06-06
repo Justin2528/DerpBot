ade30b3c4767d2eee054643394fc8b36387acb51
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-06-06 23:04:01 +0800

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -23,9 +23,16 @@ module.exports.run = async (bot, message, args) => {
         .setThumbnail(message.author.displayAvatarURL)
         .setFooter(`${moment().format('MMMM Do YYYY, h:mm:ss a')}`)
         .setColor(16711728);
-    message.channel.send({ embed: embed2 })
+    message.channel.send({ embed: embed2 });
+    const embed = new Discord.RichEmbed()
+        .setAuthor(`Ticket from ${message.author.tag}`, message.author.displayAvatarURL)
+        .addField('Ticket:', `**Report's Author:** ${message.author.tag}\n**Server:** ${guild.name}\n**Full report:** ${args}`)
+        .setThumbnail(message.author.displayAvatarURL)
+        .setColor("#ffd700")
+        .setFooter(`${moment().format('MMMM Do YYYY, h:mm:ss a')}`);
+    cnl.send({ embed })
+        .catch(e => logger.error(e))
 
-        .catch(e => logger.error(e));
 }
 
 
