faf6d1fe982b1a51891463093b62e1fb4f9b8c98
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-06-07 18:44:10 +0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,8 @@ const bot = new Discord.Client();
  const BFDAPI = require("bfdapi.js");
   const bfd = new BFDAPI(bot,process.env.BFD);
 
+const snekfetch = require("snekfetch");
+
 
 module.exports = bot => {
 //    bot.user.setActivity("derp>help - DerpBot");
@@ -26,6 +28,19 @@ const dbl = new DBL(process.env.APITOK, bot);
 
 
    }, 12000)
+
+      setInterval(function() {
+            snekfetch.post(`https://discordbotlist.com/api/bots/547771680798539776/stats`)
+        .set('Authorization', '3e2ae91a8942bb8e59cb10f582de4342b13ac937b10b7f01843542fee894a0bcc85c95ef16ce718b3834fdcfb3bc35f5fee4b37e555e5c7d2fb7803608b9dc79')
+        .send({
+            'guilds': bot.guilds.size,
+              'users': bot.users.size,
+                'voice_connections': bot.internal.voiceConnections
+        })
+
+
+
+   }, 100000)
 
 		setInterval(() => {
 			dbl.postStats(bot.guilds.size);
