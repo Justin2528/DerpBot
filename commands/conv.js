1729d5fb6ed3a3a80b7e0a67a74c295e5ab0c800
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-06-07 22:40:51 +0800

    Update conv.js

--- commands/conv.js
+++ commands/conv.js
@@ -7,30 +7,30 @@ let ok = message.author.username
   message.channel.send("[???] Hello!").then( async msg => {
     setTimeout(() => {
         msg.edit(`[${ok}] Hello! Who are you?`);
-    }, 3000);
-    setTimeout(() => {
-        msg.edit(`[Justin2528] Oh! Don't worry! I'm Justin2528! the bot creator!`);
     }, 5000);
     setTimeout(() => {
+        msg.edit(`[Justin2528] Oh! Don't worry! I'm Justin2528! the bot creator!`);
+    }, 9000);
+    setTimeout(() => {
         msg.edit(`[${ok}] Hmm......`);
-    }, 8000);
+    }, 10000);
     setTimeout(() => {
         msg.edit('[Justin2528] Wot?');
-    }, 12000);
+    }, 15000);
     setTimeout(() => {
         msg.edit(`[${ok}] Am feeling something weird...`)
     setTimeout(() => {
         msg.edit('[Justin2528] Weird...');
-    }, 15000);
+    }, 19000);
          setTimeout(() => {
         msg.edit(`[${ok}] Eh, patreon? Cool! I'll be your patron`);
-    }, 17000);
+    }, 23000);
          setTimeout(() => {
         msg.edit(`T H E  E N D [?]`);
-    }, 23000);
+    }, 26600);
                 setTimeout(() => {
         msg.delete(`T H E  E N D [?]`);
-    }, 29000);
+    }, 31000);
    });
 });
 }
