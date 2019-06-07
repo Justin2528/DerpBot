99e02b971ea0c027230fdaff9dbe2e46da45517b
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-06-07 14:28:16 +0800

    Update createinvite.js

--- commands/createinvite.js
+++ commands/createinvite.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+ if(message.channel.type === "dm") return message.channel.send("Sorry! But this command `createinvite` don't work in DM!");
     if (!message.member.hasPermission("CREATE_INSTANT_INVITE")) return message.channel.send("Nope");
   message.channel.createInvite({maxAge: 0}).then(invite => {
     let embed = new Discord.RichEmbed()
