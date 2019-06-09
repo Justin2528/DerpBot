f79e4de1893d8aafb597b93d9cf462e429743f7d
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-06-09 11:49:02 +0800

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -32,6 +32,9 @@ module.exports.run = async (bot, message, args) => {
   let mutetime = args[1];
   if(!mutetime) return message.reply("You didn't specify a time!");
 
+if(mutetime === NaN) return message.channel.send("Not a number!")
+ 
+ 
   message.delete().catch(O_o=>{});
 
   try{
@@ -47,7 +50,7 @@ module.exports.run = async (bot, message, args) => {
   .addField("Muted in", message.channel)
   .addField("Time", message.createdAt)
   .addField("Length", mutetime)
-
+   .addField("Reason" reason)
   let derp = message.guild.channels.find(`name`, "derp-logs");
   if(!derp) return message.reply("Please create a incidents channel first!");
   derp.send(muteembed);
@@ -68,7 +71,7 @@ module.exports.config = {
   name: "mute",
   aliases: ["m","nospeak"],
   description: "Mute bad bois :>",
-  usage: "d>mute <@user> <time>",
+  usage: "d>mute <@user> <time> <reason>",
   noalias: "No Aliases",
   accessableby: "Staff"
 }
