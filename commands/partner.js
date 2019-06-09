cebfefd06fe230d40746778197641f1b781e6862
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-06-09 22:30:13 +0800

    Update partner.js

--- commands/partner.js
+++ commands/partner.js
@@ -2,11 +2,16 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   //code here
+const ayy = bot.emojis.get("468044901913853963");
   let ok = new Discord.RichEmbed()
- .setTitle("Partners")
+
+
+ .setTitle("${ayy} Partners")
  .setDescription("[Hyper (Discord Server)](https://discord.gg/KVVvaqn)\n [{TheSourceCode} (Discord Server)](https://discord.gg/nbjYAvq)\n [[MenuDocs] (Discord Server)](https://discord.gg/h4X9eNY)")
  .setFooter("DerpBot Partners")
-  
+ 
+ 
+ message.channel.send(ok)
 }
 
 
