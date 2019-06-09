e071937b8912c2cab0afade7589a8e3461ce786f
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-06-09 11:36:42 +0800

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -25,9 +25,35 @@ module.exports.run = async (bot, message, args) => {
       console.log(e.stack);
     }
   }
+
+  message.delete().catch(O_o=>{});
+
+
   //end of create role
   let mutetime = args[1];
   if(!mutetime) return message.reply("You didn't specify a time!");
+
+
+  message.delete().catch(O_o=>{});
+
+  try{
+    await tomute.send(`Hi! You've been muted for ${mutetime}. Sorry!`)
+  }catch(e){
+    message.channel.send(`A user has been muted... but their DMs are locked. They will be muted for ${mutetime}`)
+  }
+
+  let muteembed = new Discord.RichEmbed()
+  .setDescription(`Mute executed by ${message.author}`)
+  .setColor("ORANGE")
+  .addField("Muted User", tomute)
+  .addField("Muted in", message.channel)
+  .addField("Time", message.createdAt)
+  .addField("Length", mutetime)
+  .addField("Reason", reason);
+
+  let derp = message.guild.channels.find(`name`, "derp-logs");
+  if(!derp) return message.reply("Please create a incidents channel first!");
+  derp.send(muteembed);
 
   await(tomute.addRole(muterole.id));
   message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
@@ -37,7 +63,7 @@ module.exports.run = async (bot, message, args) => {
     message.channel.send(`<@${tomute.id}> has been unmuted!`);
   }, ms(mutetime));
 
-  
+
 }
 
 
