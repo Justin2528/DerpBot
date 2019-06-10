8da3663288625ef8235dad89c807eeb32f052614
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-06-10 16:08:13 +0800

    Update ascii.js

--- commands/ascii.js
+++ commands/ascii.js
@@ -4,6 +4,7 @@ const ascii = require("ascii-art");
 module.exports.run = async (bot, message, args) => {
   ascii.font(args.join(' '), "Doom", function(rendered) {
       
+ if(args[0]) return message.channel.send("Nope.")
 
     rendered = rendered.trimRight();
 
