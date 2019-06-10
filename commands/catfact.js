32abb37989392c9c2d3ac7bb1523df54b06bf9be
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-06-10 23:27:17 +0800

    Create catfact.js

---
+++ commands/catfact.js
@@ -0,0 +1,26 @@
+    
+const Discord = require("discord.js");
+
+const catFacts = require('cat-facts');
+ 
+
+module.exports.run = async (bot, message, args) => {
+  let randomFact = catFacts.random();
+const ayy = bot.emojis.get("468044901913853963");
+ let ok = new Discord.RichEmbed()
+   .setTitle(`${ayy} DOG Fact!`)
+  .setColor("RANDOM")
+   .setDescription(randomFact)
+   .setFooter("Derpbot Woof");
+message.channel.send(ok)
+}
+
+
+module.exports.config = {
+  name: "catfact",
+  aliases: ["cg"],
+  description: "Cat Fact Meow",
+  usage: "d>catfact",
+  noalias: "No Aliases",
+  accessableby: "Cats or Members"
+}
