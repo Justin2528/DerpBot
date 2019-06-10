7e70991ef9575a448fe913b74f4afd88e3ca0528
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-06-10 23:15:17 +0800

    Update wikiat.js

--- commands/wikiat.js
+++ commands/wikiat.js
@@ -5,7 +5,7 @@ var WikiFakt = require('wikifakt');
 module.exports.run = async (bot, message, args) => {
 
  const ayy = bot.emojis.get("468044901913853963");
-  WikiFakt.getArticleTitle().then(function(title) {
+  WikiFakt.getRandomArticleTitle().then(function(title) {
   let ok = new Discord.RichEmbed()
   .setColor("Random")
   .setTitle(`${ayy} Random Article Title!`)
