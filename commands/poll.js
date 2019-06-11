2978e9d198996bf590a21f597d49d9df379deab3
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-06-11 16:31:15 +0800

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   //code here
+ let cool = bot.emojis.get("450936886739861504");
   if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send("NEED ADMIN PERMS!");
   
 if(!args[0]) return message.channel.send("Uh... Air?")
@@ -12,6 +13,14 @@ if(!args[0]) return message.channel.send("Uh... Air?")
   .setFooter("React to vote!")
    .setDescription(args.join(' '))
    .setTitle(`Poll Created By ${message.author.username}`)
+
+  let msg = await message.channel.send(ok);
+
+   await msg.react("✅");
+   await msg.react(cool)
+   await msg.react("❌")
+
+ message.delete()
  
 }
 
